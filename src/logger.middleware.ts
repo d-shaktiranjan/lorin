3cadead57c3e965209da8fc278c66063345f0f91
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2025-03-19 20:54:32 +0530

    chore: .editorconfig & prettier added

--- src/logger.middleware.ts
+++ src/logger.middleware.ts
@@ -26,12 +26,12 @@ const loggerMiddleware = (req: Request, res: Response, next: NextFunction) => {
             statusCode >= 500
                 ? "bgRed"
                 : statusCode >= 400
-                ? "red"
-                : statusCode >= 300
-                ? "cyan"
-                : statusCode >= 200
-                ? "green"
-                : "default";
+                  ? "red"
+                  : statusCode >= 300
+                    ? "cyan"
+                    : statusCode >= 200
+                      ? "green"
+                      : "default";
 
         logMessageToConsoleAndFile(logMessage, ".logs/api.log", color);
     });
