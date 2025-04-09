b7dd1b27c8b9b2c407c36a91a918d2650fe2d275
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2025-04-10 00:37:16 +0530

    fix: build & import issue

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,3 @@ import { logger } from "./logger.js";
 import loggerMiddleware from "./logger.middleware.js";
 
 export { logger, loggerMiddleware };
-
-// CommonJS compatibility
-export default { logger, loggerMiddleware };
